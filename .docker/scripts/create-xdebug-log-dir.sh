501c91f0a4ccebcaf171acb712690e77c27eaa84
Anver Sadutt <anvergdr@gmail.com>
2025-06-30 12:35:13 +0530

    Fix xdebug log file error when running php commands in dev env

--- .docker/scripts/create-xdebug-log-dir.sh
+++ .docker/scripts/create-xdebug-log-dir.sh
@@ -6,5 +6,8 @@ echo "Creating /var/log/xdebug directory..."
 # Ensure the directory exists and has the correct permissions
 mkdir -p /var/log/xdebug && chmod 777 /var/log/xdebug && chown www-data:www-data /var/log/xdebug
 
+# Create the Xdebug log file with the correct permissions
+touch /var/log/xdebug/xdebug.log && chmod 666 /var/log/xdebug/xdebug.log && chown www-data:www-data /var/log/xdebug/xdebug.log
+
 # Debugging: Log the completion of the script
 echo "Xdebug log directory created and permissions set."
